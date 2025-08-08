3ebd7aed5c18c4c06e68404c2b3f8958989991d6
hassan523 <hs542327@gmail.com>
2025-08-08 11:38:08 +0500

    push

--- src/screens/User/News/News.tsx
+++ src/screens/User/News/News.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import colors from '../../../utils/colors/colors';
 import Font from '../../../utils/fonts/Font';
 import { windowWidth } from '../../../utils/dimensions/dimensions';
+import GradientBG from '../../../components/GradientBG/GradientBG';
 
 const News = () => {
      const [refreshing, setRefresing] = useState(false);
@@ -15,8 +16,7 @@ const News = () => {
      };
 
      const renderItem = () => (
-          <View style={styles.NewsContainer}>
-               <Image source={require('../../../assets/bg4.png')} style={styles.IconImage} />
+          <View style={styles.Container}>
                <Text style={styles.NewsHeading}>News Heading</Text>
                <Text style={styles.Desc}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -26,31 +26,61 @@ const News = () => {
           </View>
      );
      return (
-          <FlatList
-               data={[1, 2, 3, 4, 5]}
-               renderItem={renderItem}
-               keyExtractor={item => item.toString()}
-               showsVerticalScrollIndicator={false}
-               refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} colors={[colors.PrimaryColor]} />}
-               contentContainerStyle={styles.Container}
-               ListHeaderComponent={<Text style={[styles.Heading, { textAlign: 'center' }]}>News</Text>}
-          />
+          <View style={styles.MainContainer}>
+               <GradientBG style={styles.gradient} isBackgroundImage>
+                    <Text style={{ textAlign: 'center', marginTop: 20, color: colors.SecondaryColor, fontSize: 25, fontFamily: Font.font600 }}>NEWS</Text>
+
+                    <FlatList
+                         data={[1, 2, 3, 4, 5]}
+                         renderItem={renderItem}
+                         keyExtractor={item => item.toString()}
+                         showsVerticalScrollIndicator={false}
+                         refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} colors={[colors.PrimaryColor]} />}
+                         contentContainerStyle={{ width: '100%' }}
+                    />
+               </GradientBG>
+          </View>
      );
 };
 
 export default News;
 
 const styles = StyleSheet.create({
+     MainContainer: {
+          flex: 1,
+          alignItems: 'center',
+          backgroundColor: 'red',
+          justifyContent: 'center',
+     },
      Container: {
           paddingHorizontal: 20,
           paddingTop: 15,
           gap: 25,
-          width: windowWidth,
-          paddingBottom: 40,
-          backgroundColor: 'white',
+          width: '95%',
+          paddingBottom: 20,
+          backgroundColor: 'transparent',
+          overflow: 'hidden',
+          marginTop: 15,
+          borderRadius: 15,
+          borderWidth: 1,
+          borderColor: colors.SecondaryColor,
+
+          shadowColor: '#fff',
+          shadowOffset: {
+               width: 2,
+               height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 180,
+     },
+     gradient: {
+          borderRadius: 0,
+          width: '100%',
+          height: '100%',
      },
      Heading: {
-          color: colors.textColor,
+          color: colors.SecondaryColor,
           fontFamily: Font.font600,
           fontSize: 16,
      },
@@ -78,13 +108,13 @@ const styles = StyleSheet.create({
           padding: 15,
      },
      NewsHeading: {
-          color: colors.PrimaryColor,
+          color: colors.SecondaryColor,
           fontFamily: Font.font700,
           fontSize: 18,
      },
      Desc: {
           fontFamily: Font.font500,
-          color: colors.terTextColor,
+          color: colors.SecondaryColor,
           fontSize: 16,
      },
      NewsImages: {
