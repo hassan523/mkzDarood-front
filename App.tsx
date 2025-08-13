e86cd2fd578d59c46ec964c247050d16af8d8fa3
hassan523 <hs542327@gmail.com>
2025-08-13 06:46:28 +0500

    push

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import RootNavigation from './src/navigation/RootNavigation';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -6,17 +6,22 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <SafeAreaProvider>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right']}>
-            <RootNavigation />
-          </SafeAreaView>
-        </GestureHandlerRootView>
-      </SafeAreaProvider>
-    </Provider>
-  );
+     useEffect(() => {
+          SplashScreen.hide();
+     }, []);
+
+     return (
+          <Provider store={store}>
+               <SafeAreaProvider>
+                    <GestureHandlerRootView style={{ flex: 1 }}>
+                         <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right']}>
+                              <RootNavigation />
+                         </SafeAreaView>
+                    </GestureHandlerRootView>
+               </SafeAreaProvider>
+          </Provider>
+     );
 }
