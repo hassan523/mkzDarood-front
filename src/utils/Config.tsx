3c1869dff999d2e26e7230896a7f6353f941bda1
hassan523 <hs542327@gmail.com>
2026-09-07 04:44:05 +0500

    chore: save working state before RN upgrade

--- src/utils/Config.tsx
+++ src/utils/Config.tsx
@@ -1,6 +1,6 @@
-// const API_BASE_URL = 'https://mkz-darood.vercel.app';
+const API_BASE_URL = 'https://mkz-darood.vercel.app';
 // const API_BASE_URL = 'https://mkz-darood-testing.vercel.app';
-const API_BASE_URL = 'http://192.168.0.103:5000';
+// const API_BASE_URL = 'http://192.168.0.103:5000';
 // const API_BASE_URL = 'http://172.25.224.1:5000';
 // const API_BASE_URL = 'http://192.168.192.101:5000';
 
